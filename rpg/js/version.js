6289c17e053ac7c96a97ada155b994c3f468f271
Atlas <atlas.asittley@gmail.com>
2026-03-24 18:38:59 -0400

    Add mucklo directory

--- rpg/js/version.js
+++ rpg/js/version.js
@@ -1,6 +1,6 @@
 // AUTO-GENERATED — do not edit
 const GAME_VERSION = {
-  commit: '277dbee',
-  date: '2026-03-24 22:31 UTC',
-  message: 'Update landing page with all projects'
+  commit: '48d4a25',
+  date: '2026-03-24 22:38 UTC',
+  message: 'Restore all project files'
 };
