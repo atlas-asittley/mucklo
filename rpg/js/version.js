277dbeed21a863399fc72af231f3e63c81f01388
Atlas <atlas.asittley@gmail.com>
2026-03-24 18:31:32 -0400

    Update landing page with all projects

--- rpg/js/version.js
+++ rpg/js/version.js
@@ -1,6 +1,6 @@
 // AUTO-GENERATED — do not edit
 const GAME_VERSION = {
-  commit: 'bcd3c2d',
+  commit: 'd9b1156',
   date: '2026-03-24 22:06 UTC',
-  message: 'Add Mucklo band website'
+  message: 'Add Mucklo band website (clean)'
 };
